7a975e1332c63ad54624c340444e942694d9d3a7
veryHappyApple <139455578+veryHappyApple@users.noreply.github.com>
2023-07-13 13:49:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Posts from "./components/Posts";
 
 function App() {
   return (
+    <HashRouter basename={process.env.PUBLIC_URL}>
     <div className="App">
       <Posts />
     </div>
+    </HashRouter>
   );
 }
 
